6adae56c9fe944fa983566ea91c555646f790c8b
Adam Boettcher <baboettcher@gmail.com>
2017-08-01 20:10:41 -0700

    return 404 for non-existant file

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,45 +4,42 @@ var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
 // require more modules/folders here!
 
-
-exports.handleRequest = function(req, res) {
-
-  // test the req.method to see if it is a GET or POST
-
-  // if a GET, then call handleGetRequest
-
-  // if a POST, then call handlePostRequest
-
-
-
-
-
-  fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', (err, data) => {
-    var index = archive.paths.siteAssets + '/index.html';
-    if (err) {
-      console.error('ERROR: ', err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    res.end(data);
-
+var return404 = function(res) {
+  res.writeHead(404, {
+    'Content-Type': 'text/plain'
   });
-
-
+  res.end('404 Not Found\n');
 };
-
 
 var handleGetRequest = function(req, res) {
   // find the url being requested
+  if (req.url !== '/') {
+    return404(res);
+  } else {
+    var url = archive.paths.siteAssets + '/index.html';
 
-  // test url to see if it is root '/'
 
-  // if it is root, then give it 'index.html'
-
-  // else return 404
+    fs.readFile(url, 'utf8', (err, data) =>     {
+      if (err) {
+        console.error('ERROR: ', err);
+      }
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    });
+  }
 };
+
 
 var handlePostRequest = function(req, res) {
 
+};
+
+exports.handleRequest = function(req, res) {
+  if (req.method === 'GET') {
+    handleGetRequest(req, res);
+  } else if (req.method === 'POST') {
+    handlePostRequest(req, res);
+  }
 };
