bde558781fabfeb77650d2d40e05d8c2371a5645
Adam Boettcher <baboettcher@gmail.com>
2017-08-01 19:36:23 -0700

    pseudocode and stub setup

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,14 +5,26 @@ var fs = require('fs');
 // require more modules/folders here!
 
 
-exports.handleRequest = function (req, res) {
+exports.handleRequest = function(req, res) {
+
+  // test the req.method to see if it is a GET or POST
+
+  // if a GET, then call handleGetRequest
+
+  // if a POST, then call handlePostRequest
+
+
+
+
 
   fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', (err, data) => {
     var index = archive.paths.siteAssets + '/index.html';
     if (err) {
       console.error('ERROR: ', err);
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
     res.end(data);
 
   });
@@ -21,4 +33,16 @@ exports.handleRequest = function (req, res) {
 };
 
 
+var handleGetRequest = function(req, res) {
+  // find the url being requested
 
+  // test url to see if it is root '/'
+
+  // if it is root, then give it 'index.html'
+
+  // else return 404
+};
+
+var handlePostRequest = function(req, res) {
+
+};
