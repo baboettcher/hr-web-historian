8f67f656e09cfdfc782a1313bc3eee670ba35661
Adam Boettcher <baboettcher@gmail.com>
2017-08-01 19:13:05 -0700

    returns content of html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
+var http = require('http');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', (err, data) => {
+    var index = archive.paths.siteAssets + '/index.html';
+    if (err) {
+      console.error('ERROR: ', err);
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+
+  });
+
+
 };
+
+
+
